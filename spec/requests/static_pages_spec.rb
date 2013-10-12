5b4a9cf99f35555a5e15e3d1bac4b60e8246e873
morcik <morciszon@o2.pl>
2013-10-12 13:07:31 -0700

    finished static pages tut

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,11 +7,21 @@ describe "Static Pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
   end
   describe "Help Page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
     end
   end
   describe "About Page" do
@@ -19,6 +29,10 @@ describe "Static Pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About Us")
+    end
   end
-
 end
