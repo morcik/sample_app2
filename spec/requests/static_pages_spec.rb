67a8a9e52b0e3d5279aa59b722dc06fafbeecbe7
morcik <morciszon@o2.pl>
2013-10-12 14:05:17 -0700

    bonus exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,6 +13,7 @@ describe "Static Pages" do
       expect(page).to have_title("Home")
     end
   end
+  
   describe "Help Page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
@@ -24,6 +25,7 @@ describe "Static Pages" do
       expect(page).to have_title("Help")
     end
   end
+  
   describe "About Page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
@@ -33,6 +35,17 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/about'
       expect(page).to have_title("About Us")
+    end
+  end
+  describe "Contact" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Contact")
     end
   end
 end
